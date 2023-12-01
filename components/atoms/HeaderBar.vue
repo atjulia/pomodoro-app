e8e499bcd0ba4107a8c03d8cdf4140cf71c988ee
julia.karolina <julia.karolina@fulltechpro.com.br>
2023-12-01 16:34:07 -0300

    fix headerbar

--- components/atoms/HeaderBar.vue
+++ components/atoms/HeaderBar.vue
@@ -1,5 +1,5 @@
 <template functional>
-  <div class="flex flex-row justify-between pb-2">
+  <div class="pb-2 flex flex-row justify-between">
     <img src="/logo-full.svg" :style="{ maxHeight: '40px'}">
   </div>
 </template>
