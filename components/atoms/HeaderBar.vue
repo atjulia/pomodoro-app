b93d68105da174e0b52e5c70f25ac9cf18ebfbde
julia.karolina <julia.karolina@fulltechpro.com.br>
2023-12-01 16:32:27 -0300

    fix headerbar

--- components/atoms/HeaderBar.vue
+++ components/atoms/HeaderBar.vue
@@ -1,25 +1,5 @@
-<template>
+<template functional>
   <div class="flex flex-row justify-between pb-2">
     <img src="/logo-full.svg" :style="{ maxHeight: '40px'}">
-    <button @click="setModalUp" class="flex flex-row items-center justify-center gap-x-1 text-white bg-red hover:bg-red-dark rounded-md px-2"><img src="/icons/gear.svg" :style="{ maxHeight: '20px'}">settings</button>
-    <SettingsModal />
   </div>
 </template>
-
-<script lang="ts">
-import Vue from 'vue'
-import { mapMutations } from 'vuex';
-import SettingsModal from '@/components/atoms/SettingsModal.vue'
-import { Mutations as CountdownMT } from '~/store/countdown/types';
-export default Vue.extend({
-  components: { SettingsModal },
-  methods: {
-		...mapMutations({
-			ModalUp: `countdown/${CountdownMT.SET_SETTINGS_UP_MODAL_OPEN}`,
-		}),
-    setModalUp () {
-      console.log(this.ModalUp)
-    }
-  }
-})
-</script>
